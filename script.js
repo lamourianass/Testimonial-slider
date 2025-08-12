9728fe1ddd1a85ea3109f5109710192802718e41
Anass Lamouri <anasslamouri@icloud.com>
2025-08-12 14:49:38 +0100

    add object to testimanial array in js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-const testimonials = []
+const testimonials = [
+  {
+    name: "Emily Johnson",
+    photoUrl: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    testimonial: "I am extremely satisfied with the exceptional quality of mobile phones from apple."
+  }
+]
