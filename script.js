a54f0a121740ba3bc4d8f6accd3d79bfaacff5ce
Anass Lamouri <anasslamouri@icloud.com>
2025-08-12 15:17:08 +0100

    const imgEl in js file

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 import { testimonials } from './testimonials.js'
 
+const imgEl = document.querySelector('img')
+
+
 let index = 0
 
 function updateTestimonial() {
-  const {name, photoUrl, testimonial} = testimonials
+  const {name, photoUrl, testimonial} = testimonials[index]
 }
